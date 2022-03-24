f47f7678fcc1a9cced0f84dfeaa85f5305e06ebc
tyronmaster <degonski.art@gmail.com>
2022-03-24 10:02:45 +0200

    feat: add carbon dating realisation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof (sampleActivity) != 'string' || !Number(sampleActivity) || Number(sampleActivity) <= 0 || Number(sampleActivity) > 15) {
+    return false;
+  }
+  const k = .693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 }
 
 module.exports = {
