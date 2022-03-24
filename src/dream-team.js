6d90e5c12610616087b8b1a29181e98de3b51cfc
tyronmaster <degonski.art@gmail.com>
2022-03-24 12:26:22 +0200

    feat: add dream-team realisation

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!members || !Array.isArray(members)) return false;
+  members = members.filter(item => typeof (item) == 'string').map(item => item.split(' ').join(''));
+  return members.map(item => item.toUpperCase().split('').filter((item, index) => index == 0).join()).sort().join('');
 }
 
 module.exports = {
