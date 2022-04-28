b18adf2dae7afd67ccbd93961b4a04576c017606
tyronmaster <degonski.art@gmail.com>
2022-04-28 16:14:00 +0200

    feat: add recursive depth realisation

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) return 1;
   }
 }
 
