1e3b2edd8c283cc44477aafb2efe8318d8492a93
tyronmaster <degonski.art@gmail.com>
2022-05-02 21:58:51 +0300

    feat: add extended-repeater.js code

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+str = String(str);
+Object.keys(options).includes('repeatTimes') ? options.repeatTimes = Number(options.repeatTimes) : options.repeatTimes = 1;
+Object.keys(options).includes('separator') ? options.repeatTimes = String(options.repeatTimes) : options.separator = "+";
+Object.keys(options).includes('addition') ? options.addition = String(options.addition) : options.addition = "";
+Object.keys(options).includes('additionRepeatTimes') ? options.additionRepeatTimes = Number(options.additionRepeatTimes) : options.additionRepeatTimes = 1;
+Object.keys(options).includes('additionSeparator') ? options.additionSeparator = String(options.additionSeparator) : options.additionSeparator = "|";
+// build addition string
+let additionString = '';
+for(let i = 1; i < options.additionRepeatTimes; i++){
+  additionString += `${options.addition}${options.additionSeparator}`;
+}
+additionString += `${options.addition}`;
+
+//build main string + addition string
+let mainString = '';
+for(let i = 1; i < options.repeatTimes; i++){
+  mainString += `${str}${additionString}${options.separator}`;
+}
+mainString += `${str}${additionString}`;
+return mainString;
 }
 
 module.exports = {
